33c61448a69d1c24cdb2ecc564d502f4e953264b
Shujaat Azim <shujaat.azim@gmail.com>
2019-05-09 03:27:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   new_arr = []
   splitties = string.split(" ")
   splitties.each do |word|
-    new_arr << word.reverse()
+    new_arr << word.to_s.reverse()
   end
   puts new_arr.join(" ")
 end
